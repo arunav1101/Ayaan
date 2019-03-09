c3f0f2bd564f8258653f556a9b476ff7e1c63dde
Arunav Gupta <arunav.gupta@nike.com>
2019-03-08 16:49:24 -0800

    initial

--- logic.js
+++ logic.js
@@ -16,3 +16,9 @@ $("#prePage").on("click",function(e){
     e.preventDefault();
     open("index.html")
 })
+
+$("#page1").on("click",function(e){
+    e.preventDefault();
+    open("page1.html")
+})
+
